3a3e9b2e39ad3a00fbe6753bf81c4fce9c8ba3d6
vladislav3112 <dyushakolesov@mail.ru>
2023-03-15 00:44:51 +0000

    task done

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## aaa-backend homework
 ### to build use `docker build -t ds-backend .`
-### use `run.sh` to run application on http://127.0.0.1:8080
+### use `run.sh` or `docker run -v $PWD:/app -p 8080:8080 ds-backend` to run application on http://127.0.0.1:8080
 ### go to /images/<int:img_id> to get plate image number
--- src/app.py
+++ src/app.py
@@ -20,9 +20,12 @@ def hello():
 # {"plate_number": "c180mv ..."}
 @app.route('/readPlateNumber', methods=['POST'])
 def read_plate_number():
-    im = request.get_data()
-    im = io.BytesIO(im)
-
+    try:
+        im = request.get_data()
+        im = io.BytesIO(im)
+    except UnicodeEncodeError:
+        logging.error('UnicodeEncodeError')
+        return {'error': 'UnicodeEncodeError'}, 400
     try:
         res = plate_reader.read_text(im)
     except InvalidImage:
@@ -50,6 +53,35 @@ def read_img_by_id(img_id):
     except JSONDecodeError:
         return 'Auto plate picture not found', 404
     return res
+
+@app.route('/images/<int:img_id1>/<int:img_id2>')
+def read_img_by_multiple_id(img_id1, img_id2):
+    url1 = f'http://51.250.83.169:7878/images/{str(img_id1)}'
+    url2 = f'http://51.250.83.169:7878/images/{str(img_id2)}'
+    client = PlateReaderClient(host='http://127.0.0.1:8080')
+    if img_id1 != 10022 and img_id1 != 9965:
+        return 'invalid image id1', 400
+    if img_id2 != 10022 and img_id2 != 9965:
+        return 'invalid image id2', 400
+    try:
+        im = requests.get(url1, timeout=5)
+        res1 = client.read_plate_number(im)
+        print('res1: ',res1)
+        im = requests.get(url2, timeout=5)
+        res2 = client.read_plate_number(im)
+        print('res2: ',res2)
+    except ConnectTimeout:
+        return 'Auto plate picture service unavaliable (request timeout)', 416    
+    except ConnectionError:
+        return 'Auto plate picture service unavaliable', 503
+    except JSONDecodeError:
+        return 'Auto plate picture not found', 404
+    result = {
+        'plate_number1': res1['plate_number'],
+        'plate_number2': res2['plate_number']
+    }
+    
+    return result
 
 if __name__ == '__main__':
     logging.basicConfig(
